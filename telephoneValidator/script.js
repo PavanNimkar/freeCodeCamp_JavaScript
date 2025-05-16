af380aea0125f3accf5792fcf59f57d2713ae6fb
PavanNimkar <pavannnimkar@gmail.com>
2025-05-16 22:45:48 +0530

    added telephoneValidator

---
+++ telephoneValidator/script.js
@@ -0,0 +1,20 @@
+const checkBtn = document.querySelector("#check-btn");
+const clearBtn = document.querySelector("#clear-btn");
+let results = document.querySelector("#results-div");
+let input = document.querySelector("#user-input");
+
+clearBtn.addEventListener("click", () => {
+  results.innerText = "";
+});
+// /^(1\s?)?(\(\d{3}\)|\d{3})([\s-]?)\d{3}([\s-]?)\d{4}$/
+checkBtn.addEventListener("click", () => {
+  const regex = /^(1\s?)?(\(\d{3}\)|\d{3})([\s-]?)\d{3}([\s-]?)\d{4}$/;
+  if (!input.value) {
+    alert("Please provide a phone number");
+  } else if (regex.test(input.value)) {
+    results.innerText = `Valid US number: ${input.value}`;
+  } else {
+    results.innerText = `Invalid US number: ${input.value}`;
+  }
+  input.value = "";
+});
